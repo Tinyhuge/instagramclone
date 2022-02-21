48817063c112cd903dffc511b91422ba29fc8abb
iamrahul <gunjansharma1112info@gmail.com>
2022-02-21 15:42:52 +0530

    Started Building Instagram Clone

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import react from "react";
+import "../css/App.css";
+
+function App() {
+  return (
+    <div>
+      <h2>Let's Build Instagram Clone..!!</h2>
+    </div>
+  );
+}
+
+export default App;
